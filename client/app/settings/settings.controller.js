4ac905b5d5709535b0578b071ae0bd9853536920
Michael Staub <mikestaub22@gmail.com>
2015-03-20 15:46:15 -0700

    fixed settings navigation

--- client/app/settings/settings.controller.js
+++ client/app/settings/settings.controller.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('friendfinderApp')
-  .controller('SettingsCtrl', function ($scope, User, Auth) {
+  .controller('SettingsCtrl', function ($scope, User, Auth, $state) {
     $('.settings-container .menu .item')
       .tab({
         context : '.settings-container',
         // history: true
-      })
-    ;
+      });
+
+      $("[ui-sref='"+$state.current.name+"']").addClass('active');
+      $("[data-tab='"+$state.current.name.split('.')[1]+"']").addClass('active');
   });
